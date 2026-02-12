0cb538651fc078ce675a7e9aef7e89499c8c827d
DavidMismas <100374656+DavidMismas@users.noreply.github.com>
2026-02-12 06:07:54 +0100

    watch app working, B5

---
+++ TrailMindWatch Watch App/TrailMindWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrailMindWatchApp.swift
+//  TrailMindWatch Watch App
+//
+//  Created by David Mišmaš on 11. 2. 26.
+//
+
+import SwiftUI
+
+@main
+struct TrailMindWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
